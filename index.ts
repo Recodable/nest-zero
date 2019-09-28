aa562d2315da841e35051b242f36eafd483f5ea7
Steven Yung <stevenyung@icloud.com>
2019-09-28 21:45:15 +0200

    init

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './nest-zero-application';
+export * from './nest-zero-factory';
+
+export * from './nest-zero-controller.decorator';
+export * from './nest-zero-command.decorator';
---
+++ nest-zero-application.ts
@@ -0,0 +1,45 @@
+import { INestApplicationContext } from '@nestjs/common';
+import * as commander from 'commander';
+import {
+  NestContainer,
+  ApplicationConfig,
+  NestApplicationContext,
+} from '@nestjs/core';
+import { RoutesResolver } from './routes-resolver';
+
+export class NestZeroApplication extends NestApplicationContext {
+  private instance = commander;
+  private routesResolver: RoutesResolver;
+
+  constructor(
+    container: NestContainer,
+    private readonly config: ApplicationConfig,
+    options?,
+  ) {
+    super(container);
+
+    this.routesResolver = new RoutesResolver(
+      this.container,
+      this.config,
+      this.injector,
+    );
+  }
+
+  public async execute(args: any[]) {
+    await this.initialize();
+    this.instance.parse(args);
+  }
+
+  private async initialize() {
+    this.routesResolver.resolve(this);
+  }
+
+  public addCommand(route, controllerName: string) {
+    const controller = this.get(controllerName);
+    const signature = [route.prefix, route.routePath].filter(v => v).join(':');
+    (signature ? this.instance.command(signature) : this.instance).action(
+      controller[route.methodName].bind(controller),
+    );
+    return this;
+  }
+}
